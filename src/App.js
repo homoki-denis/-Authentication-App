abb32b19a7893b395764eb76ff56e73ed53b1ce2
Denis Homoki <homoki.denisth@gmail.com>
2022-07-16 22:46:06 +0300

    added changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.scss";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import UserProfile from "./components/UserProfile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      <h1>User Login Authentication</h1>
-      <p>Please Login To View Your Data</p>
+      {isAuthenticated && (
+        <div>
+          <h1>User Login Authentication</h1>
+          <p>Please Login To View Your Data</p>
+        </div>
+      )}
       <LoginButton />
       <UserProfile />
       <LogoutButton />
