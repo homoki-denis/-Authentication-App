9d998992a1f4a2e4caaa562fe3dbe65f7cdc79f2
Denis Homoki <homoki.denisth@gmail.com>
2022-07-16 22:50:11 +0300

    added changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       {!isAuthenticated && (
-        <div>
+        <div className="main-header">
           <h1>User Login Authentication</h1>
           <p>Please Login To View Your Data</p>
         </div>
